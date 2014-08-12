49cf9eae289837b85b2ed3d01cff12579f2bb917
Duncan Campbell <duncan.campbell@yale.edu>
2014-08-12 19:28:07 -0400

    added random creation routine

---
+++ randoms/make_all_random_catalogues.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#run random tile catalogue generator for each field
+python make_cfhtlens_random_cat.py W1 &
+wait
+python make_cfhtlens_random_cat.py W2 &
+wait
+python make_cfhtlens_random_cat.py W3 &
+wait
+python make_cfhtlens_random_cat.py W4 &
+
+#after all of those are complete, clean up the filenames
+wait
+/scratch/dac29/output/analysis/cfhtlens/clean_up_filenames.sh &
+
+#now, combine each tile catalogue into field catalogues
+wait
+python combine_cfhtlens_random_cats.py W1 &
+wait
+python combine_cfhtlens_random_cats.py W2 &
+wait
+python combine_cfhtlens_random_cats.py W3 &
+wait
+python combine_cfhtlens_random_cats.py W4 &
---
+++ randoms/make_cfhtlens_random_cat.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+
+#Author: Duncan Campbell
+#Written: August 12, 2013
+#Yale University
+#Description: make a random catalogue for each tile in the cfhtlens survey
+
+###packages###
+import numpy as np
+from astropy.io import fits
+from astropy import wcs
+import os
+import fnmatch
+import sys
+import custom_utilities as cu
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import multiprocessing
+from multiprocessing import Pool
+
+
+def main():
+    global filepath
+    global savepath
+    filepath = cu.get_data_path() + 'CFHTLens/masks/'
+    savepath = cu.get_output_path() + 'analysis/cfhtlens/'
+
+    field = sys.argv[1]
+    print 'field:', field
+
+    filenames = os.listdir('/scratch/dac29/data/CFHTLens/masks/')
+    filenames = fnmatch.filter(filenames, field+'*.fits')
+
+    p = Pool(8)
+    p.map(do_work,filenames)
+
+
+def do_work(filename):
+        print filename
+        hdulist = fits.open(filepath+filename, memmap=True)
+        data = hdulist[0].data
+        header = hdulist[0].header
+        nxpix = header['NAXIS1']
+        nypix = header['NAXIS2']
+        center_xpix = header['CRPIX1']
+        center_ypix = header['CRPIX2']
+        tile  = header['OBJECT']
+        w = wcs.WCS(hdulist[0].header)
+
+        #randomly sample the tile area
+        N_points = 1000000
+        n_points = 2.0*N_points
+        center_world = w.wcs_pix2world([[center_xpix,center_ypix]], 1)
+        center_ra = center_world[0][0]
+        center_dec = center_world[0][1]
+        ran_coords = cu.sample_spherical_cap(center_ra, center_dec, 0.75, n_points)
+        center_ra = center_ra*np.ones(n_points) #convert to array to pass through map
+        center_dec = center_dec*np.ones(n_points) #convert to array to pass through map
+        ran_ra = np.array(zip(*ran_coords)[0])
+        ran_dec = np.array(zip(*ran_coords)[1])
+        ran_coords = np.array(ran_coords)
+
+        #determine if the points are inside the tile area
+        result  = np.array(map(cu.within_tile, ran_ra, ran_dec, center_ra, center_dec))
+        #trim down the sample to the desired number of points
+        #ran_ra = ran_ra[result][0:N_points]
+        #ran_dec = ran_dec[result][0:N_points]
+        #ran_coords = ran_coords[result][0:N_points]
+        ran_ra = ran_ra[result]
+        ran_dec = ran_dec[result]
+        ran_coords = ran_coords[result]
+        #determine how many tiles the point is within(overlap regions)
+        num_tile = np.array(map(cu.num_tile, ran_ra, ran_dec))
+
+        #find pixel locations of random points
+        ran_coords = np.array(ran_coords)
+        ran_pix = w.wcs_world2pix(ran_coords, 0)
+        ran_pix = ran_pix.astype(int)
+        ran_xpix = np.array(zip(*ran_pix)[0])
+        ran_ypix = np.array(zip(*ran_pix)[1])
+
+        #create an array and save random catalogue
+        dtype = [('ra',float),('dec',float),('flag',int),('num_tile',int)]
+        randoms = np.recarray((len(ran_ra),), dtype = dtype)
+        randoms['ra'] = ran_ra
+        randoms['dec'] = ran_dec
+        randoms['flag'] = data[ran_ypix, ran_xpix] #row,column ---> y,x
+        randoms['num_tile'] = num_tile
+        print len(randoms)
+
+        np.save(savepath+tile+'_randoms', randoms)
+
+        hdulist.close()
+        return 0
+
+
+
+if __name__ == '__main__':
+    main()
